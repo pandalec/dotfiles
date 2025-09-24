a3213248db41177a3fe63181a9e61d5b1d7204de
Panda <carsten@schaellig.de>
2025-09-24 22:20:39 +0200

    Update .dotfiles

--- .config/nvim/lua/core/autocmds.lua
+++ .config/nvim/lua/core/autocmds.lua
@@ -13,3 +13,15 @@ vim.api.nvim_create_autocmd("FileType", {
     vim.opt_local.spelllang = { "de", "en_us" }
   end,
 })
+
+-- Move macro recording message to extui's box
+vim.api.nvim_create_autocmd({ "RecordingEnter", "RecordingLeave" }, {
+  callback = function(args)
+    local reg = vim.fn.reg_recording() -- get the current recording register
+    if args.event == "RecordingEnter" then
+      vim.notify("Recording @" .. reg, vim.log.levels.INFO, { timeout = 1000 })
+    else
+      vim.notify("Stopped recording @" .. reg, vim.log.levels.INFO, { timeout = 500 })
+    end
+  end,
+})
--- .config/nvim/lua/plugins/slimline.lua
+++ .config/nvim/lua/plugins/slimline.lua
@@ -2,11 +2,33 @@ vim.pack.add({
   { src = "https://github.com/sschleemilch/slimline.nvim" },
 })
 
+-- -- in your slimline config
+-- local function recording_segment()
+--   local reg = vim.fn.reg_recording()
+--   if reg ~= "" then return "Recording @" .. reg end
+--   return ""
+-- end
+
 -- Setup slimline status line
 require("slimline").setup({
   style = "fg",
   bold = true,
   hl = { secondary = "Comment" },
+  -- components = {
+  --   left = {
+  --     "mode",
+  --     "path",
+  --     "git",
+  --   },
+  --   center = {
+  --     recording_segment,
+  --   },
+  --   right = {
+  --     "diagnostics",
+  --     "filetype_lsp",
+  --     "progress",
+  --   },
+  -- },
   configs = {
     mode = {
       verbose = true,
